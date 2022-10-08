9b9cd714a88570002d9b99791f5136e49bd61d4a
AppTimDev <apptimdev@gmail.com>
2022-10-08 14:44:26 +0800

    no message

---
+++ password.lua
@@ -0,0 +1,19 @@
+function GeneratePassword(str)
+    -- convert each characters of a string to its ascii number
+    -- concat all the ascii number into a string
+    local size = string.len(str)
+    local ans = ""
+    --ans = ""
+    for i=1,size do
+        local b=string.byte(str,i)
+        ans = ans..b
+        --print(ans)
+    end
+    return ans
+end
+ 
+
+password = GeneratePassword("abc")
+print(password)
+print(type(password))
+
